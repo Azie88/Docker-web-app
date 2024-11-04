d0e6fab3a670b8427954a1f6e5b38d32a5d1d192
Azie88 <obandoandrew8@gmail.com>
2024-11-04 11:23:14 +0300

    Updated version2 PHP form

--- version2/index.php
+++ version2/index.php
@@ -4,29 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Login Form 1</h1>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username" required><br><br>
-        
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password" required><br><br>
-        
-        <input type="submit" value="Submit">
-    </form>
+    <div class="container">
+        <h1>Login Form</h1>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            
+            <input type="submit" value="Submit">
+        </form>
 
-    <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
 
-        echo "Username: " . $username . "<br>";
-        echo "Password: " . $password . "<br>";
-
-    }
-    ?>
+            echo "<p>Username: " . htmlspecialchars($username) . "</p>";
+            echo "<p>Password: " . htmlspecialchars($password) . "</p>";
+        }
+        ?>
+    </div>
 </body>
 </html>
